c93bd282fc9df565c9ac34fa5be096d1b473fe37
Thomas Hein <thomas@onlineth.com>
2019-08-03 17:16:11 -0500

    Initial commit, 🎉

---
+++ main.js
@@ -0,0 +1,52 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+const path = require('path')
+
+let mainWindow
+
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+    app.quit()
+} else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        mainWindow.show();
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) mainWindow.restore();
+            mainWindow.focus();
+        }
+    })
+
+    app.on('ready', () => {
+        app.setAppUserModelId("7185ThomasHein.MessagesbyGoogle");
+        createWindow()
+    });
+}
+
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            devTools: false,
+            nodeIntegration: false
+        },
+        icon: "assets/icon.png"
+    })
+
+    mainWindow.setMenu(null)
+    mainWindow.loadURL('https://messages.google.com/web/')
+
+    mainWindow.on('close', function(event) {
+        event.preventDefault();
+        mainWindow.hide();
+    })
+}
+
+
+app.on('activate', function() {
+    if (mainWindow === null) createWindow()
+})
