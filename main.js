4efd9345a82a5e0daf241f865a503fa021ae21c0
Noah Halstead <nhalstead00@gmail.com>
2019-09-09 22:32:03 -0400

    Code Cleanup and ignore update
    Removed extra gaps (space to tab) and removed extra vars not in use.

--- main.js
+++ main.js
@@ -1,52 +1,45 @@
-const {
-    app,
-    BrowserWindow
-} = require('electron')
-const path = require('path')
-
-let mainWindow
+const {app, BrowserWindow} = require('electron');
+let mainWindow;
 
 const gotTheLock = app.requestSingleInstanceLock()
 
 if (!gotTheLock) {
-    app.quit()
+  app.quit()
 } else {
-    app.on('second-instance', (event, commandLine, workingDirectory) => {
-        mainWindow.show();
-        if (mainWindow) {
-            if (mainWindow.isMinimized()) mainWindow.restore();
-            mainWindow.focus();
-        }
-    })
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    mainWindow.show();
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
 
-    app.on('ready', () => {
-        app.setAppUserModelId("7185ThomasHein.MessagesbyGoogle");
-        createWindow()
-    });
+  app.on('ready', () => {
+    app.setAppUserModelId("7185ThomasHein.MessagesbyGoogle");
+    createWindow()
+  });
 }
-
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            devTools: false,
-            nodeIntegration: false
-        },
-        icon: "assets/icon.png"
-    })
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      devTools: false,
+      nodeIntegration: false
+    },
+    icon: "assets/icon.png"
+  });
 
-    mainWindow.setMenu(null)
-    mainWindow.loadURL('https://messages.google.com/web/')
+  mainWindow.setMenu(null);
+  mainWindow.loadURL('https://messages.google.com/web/');
 
-    mainWindow.on('close', function(event) {
-        event.preventDefault();
-        mainWindow.hide();
-    })
+  mainWindow.on('close', function(event) {
+    event.preventDefault();
+    mainWindow.hide();
+  })
 }
 
-
 app.on('activate', function() {
-    if (mainWindow === null) createWindow()
-})
+	if (mainWindow === null) createWindow()
+});
