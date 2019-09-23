c8025c7fdb5ca81efd8d1daa66bc2cdadc998662
Thomas Hein <thomas@onlineth.com>
2019-09-23 13:33:13 -0500

    Formatting
    According to Jetbrain's Webstorm Formatting

--- src/main.js
+++ src/main.js
@@ -1,86 +1,90 @@
-const { app, BrowserWindow,Menu, Tray, nativeImage} = require('electron');
+const {app, BrowserWindow, Menu, Tray, nativeImage} = require('electron');
 let mainWindow, tray;
 
-const gotTheLock = app.requestSingleInstanceLock()
+const gotTheLock = app.requestSingleInstanceLock();
+
 
 if (!gotTheLock) {
-  app.quit()
+    app.quit()
 } else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
-	  showApp()
-  });
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        showApp()
+    });
 
-  app.on('ready', () => {
-    app.setAppUserModelId('7185ThomasHein.MessagesByGoogle');
-    createWindow();
-	  createTaskTray();
-  });
+    app.on('ready', () => {
+        app.setAppUserModelId('7185ThomasHein.MessagesByGoogle');
+        createWindow();
+        createTaskTray();
+    });
 }
+
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      devTools: false,
-      nodeIntegration: false
-    },
-    icon: 'src/assets/icon.png'
-  });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            devTools: false,
+            nodeIntegration: false
+        },
+        icon: 'src/assets/icon.png'
+    });
 
-  mainWindow.setMenu(null);
-  mainWindow.setTitle('Messages for Google');
-  mainWindow.loadURL('https://messages.google.com/web/');
+    mainWindow.setMenu(null);
+    mainWindow.setTitle('Messages for Google');
+    mainWindow.loadURL('https://messages.google.com/web/');
 
-  mainWindow.on('close', function(event) {
-  	if(process.platform === 'win32') {
-			event.preventDefault();
-			mainWindow.hide();
-		}
-  })
+    mainWindow.on('close', function (event) {
+        if (process.platform === 'win32') {
+            event.preventDefault();
+            mainWindow.hide();
+        }
+    })
 }
 
+
 function createTaskTray() {
-	try {
-		const trayIcon = nativeImage.createFromPath('src/assets/icon.ico')
-		tray = new Tray(trayIcon);
+    try {
+        const trayIcon = nativeImage.createFromPath('src/assets/icon.ico')
+        tray = new Tray(trayIcon);
 
-		tray.setToolTip('Messages by Google');
-		tray.on('click', showApp);
-		tray.on('double-click', showApp);
+        tray.setToolTip('Messages by Google');
+        tray.on('click', showApp);
+        tray.on('double-click', showApp);
 
-		const contextMenu = Menu.buildFromTemplate([
-			{label: 'Reload App', click: reloadApp},
-			{type: 'separator'},
-			{label: 'Show', click: showApp},
-			{label: 'Exit', click: exitApp}
-		]);
-		tray.setContextMenu(contextMenu);
-	}
-	catch(e) {
-		// Failed to create Tray Icon
-		console.log('Failed to Create Icon', e);
-	}
+        const contextMenu = Menu.buildFromTemplate([
+            {label: 'Reload App', click: reloadApp},
+            {type: 'separator'},
+            {label: 'Show', click: showApp},
+            {label: 'Exit', click: exitApp}
+        ]);
+        tray.setContextMenu(contextMenu);
+    } catch (e) {
+        // Failed to create Tray Icon
+        console.log('Failed to Create Icon', e);
+    }
 
 }
 
 function reloadApp() {
-	mainWindow.webContents.reload();
+    mainWindow.webContents.reload();
 }
 
+
 function showApp() {
-	mainWindow.show();
-	if (mainWindow) {
-		if (mainWindow.isMinimized()) mainWindow.restore();
-		mainWindow.focus();
-	}
+    mainWindow.show();
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.focus();
+    }
 }
 
 function exitApp() {
-	mainWindow.close();
-	app.exit();
+    mainWindow.close();
+    app.exit();
 }
 
-app.on('activate', function() {
-	if (mainWindow === null) createWindow()
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow()
 });
